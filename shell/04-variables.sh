5629aafd613885b01a7a0583565343f4957989bc
ram <chakradhar.thatikonda933@gmail.com>
2023-03-07 19:03:24 +0530

    date

--- shell/04-variables.sh
+++ shell/04-variables.sh
@@ -6,3 +6,7 @@ Date=07-03-2023
 echo "hello good evening , today date is ${Date}"
 Time=5.35pm
 echo "Current time is $Time"
+
+#command Substitution
+Date=$(date)
+echo "hello ge , today date is ${Date}"

ceded8d7a0e509ce9403dffe11b4cb1842f88499
ram <chakradhar.thatikonda933@gmail.com>
2023-03-07 17:05:17 +0530

    printing colour output

--- shell/03-printing.sh
+++ shell/03-printing.sh
@@ -1,2 +1,19 @@
 echo hello world
 echo hai caller
+
+
+# printing the colour output
+##colour syntax
+# echo -e "\e[COlm message \e[0m"
+#echo -e = Enable the colours from echo command.
+#\e[COLm = Enable aparticular colour.
+#colour        codes
+# red           31
+# green         32
+#yellow         33
+#blue           34
+#magenta        35
+#cyan           36
+
+# \e[0m = Zero code is going to reset the colour, Meaning if we enable colour for sure we need to disable it,
+otherwise colour will continue on screen.

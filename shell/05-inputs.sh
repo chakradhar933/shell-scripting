dc2ea5b43edd6443f68824f9ebe05e9bc5877b52
ram <chakradhar.thatikonda933@gmail.com>
2023-03-14 15:22:03 +0530

    add

--- shell/05-inputs.sh
+++ shell/05-inputs.sh
@@ -1,2 +1,9 @@
 # 0-n are the arguments ,$*, $@
-echo $1
+echo first argument $1 # 1st argument
+echo 2nd argument $2 # 2nd argument
+echo script name $0 # script name
+
+echo all arguments $*
+echo all arguments $@ # all arguments
+
+echo number of arguments $# # number of arguments.
